9fa60c08e3d964fe142e0942de9882e7b02439ee
Placide <zigiraplacide@gmail.com>
2025-08-06 11:18:20 +0200

    JPA Repo has been initiated

--- src/main/java/com/placide/bookapi/Repository/bookRepository.java
+++ src/main/java/com/placide/bookapi/Repository/bookRepository.java
@@ -1,11 +1,15 @@
 package com.placide.bookapi.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.placide.bookapi.Models.bookModel;
 
+@Repository
 public interface bookRepository extends JpaRepository<bookModel, Long> {
-    // This interface will automatically provide CRUD operations for bookModel
-    // Additional custom query methods can be defined here if needed
+  
+
+   
     
 }
